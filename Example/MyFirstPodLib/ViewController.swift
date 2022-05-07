db2ffe0755918a1594e5d5ffcc8036c304dea6d2
leoho0722 <leo160918@gmail.com>
2022-05-07 20:48:01 +0800

    MyFirstPodLib version 0.0.1 Example

--- Example/MyFirstPodLib/ViewController.swift
+++ Example/MyFirstPodLib/ViewController.swift
@@ -7,12 +7,17 @@
 //
 
 import UIKit
+import MyFirstPodLib
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        TestFunc.sayHello()
+        
+        TestFunc.inputAndOutput(input: "Hello World!")
     }
 
     override func didReceiveMemoryWarning() {
---
+++ Example/Pods/MyFirstPodLib/MyFirstPodLib/Classes/TestFunc.swift
@@ -0,0 +1,20 @@
+//
+//  TestFunc.swift
+//  MyFirstPodLib
+//
+//  Created by Leo Ho on 2022/5/7.
+//
+
+import Foundation
+
+public class TestFunc {
+    
+    public static func sayHello() {
+        print("MyFirstPodLib say Hello！")
+    }
+    
+    public static func inputAndOutput(input: String?) {
+        print("MyFirstPodLib：You input \(String(describing: input)), and I output \(String(describing: input))")
+    }
+    
+}
