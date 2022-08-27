9f6037c30450e8fa4515152239dc89d54107a870
leoho0722 <leo160918@gmail.com>
2022-08-28 00:52:37 +0800

    Add Swift Package Manager Support

---
+++ Example/Sources/MyFirstPodLib/SwiftPackageManagerTestFunc.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  
+//
+//  Created by Leo Ho on 2022/8/28.
+//
+
+import Foundation
+
+public class SwiftPackageManagerTestFunc {
+    
+    public static func sayHello() {
+        print("MyFirstSwiftPackage say Hello！")
+    }
+    
+    public static func inputAndOutput(input: String?) {
+        print("MyFirstSwiftPackage：You input \(String(describing: input)), and I output \(String(describing: input))")
+    }
+    
+}
---
+++ Sources/MyFirstPodLib/MyFirstPodLib.swift
@@ -0,0 +1,6 @@
+public struct MyFirstPodLib {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
